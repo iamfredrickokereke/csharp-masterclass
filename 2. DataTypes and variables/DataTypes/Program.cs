6ef6d62e5a85b193b7e469166c7d8c29cdbe5093
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-11-28 20:18:29 +0200

    datatypes

--- 2. DataTypes and variables/DataTypes/Program.cs
+++ 2. DataTypes and variables/DataTypes/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataTypes
+namespace intFloatDOuble
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int num1 = 13;
+            int num2 = 45;
+            int num3, num4, num5;
+            int sum = num1 + num2;
+
+            double d1 = 3.5;
+            double d2 = 1.54;
+            double sumD = d1 / d2;
+
+            float f1 = 3.5F;
+            // Use casting
+            //int divDandI = d1 / num2;
+
+            num3 = 15;
+            
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+            Console.WriteLine(d1 + " divided by " + d2 + " is " + sumD);
+            Console.Read();
         }
     }
 }
---
+++ 2. DataTypes and variables/strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Mantas";
+            string message = "My name is " + myName;
+            string capsMessage = message.ToUpper();
+
+            string greet = "Hello " + myName;
+            string lGreet = greet.ToLower();
+
+            Console.WriteLine(capsMessage);
+            Console.WriteLine(lGreet);
+            Console.Read();
+        }
+    }
+}
