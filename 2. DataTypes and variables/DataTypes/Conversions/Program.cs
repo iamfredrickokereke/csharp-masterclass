c6aab82c8f4fe196f5b8679dda02f68c2dce2df9
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-11-28 20:36:56 +0200

    conversions

--- 2. DataTypes and variables/DataTypes/Conversions/Program.cs
+++ 2. DataTypes and variables/DataTypes/Conversions/Program.cs
@@ -10,6 +10,29 @@ namespace Conversions
     {
         static void Main(string[] args)
         {
+            // implict conversion
+            int num = 123245436;
+            long biigNum = num;
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+
+            double myDouble = 13.37;
+            int myInt;
+
+            // explict conversion
+            myInt = (int)myDouble;
+
+            // typeConversion
+            string myString = myDouble.ToString();
+            string myFloatString = myFloat.ToString();
+            bool sunIsShining = false;
+
+            string myBool = sunIsShining.ToString();
+
+            Console.WriteLine(myBool);
+            Console.WriteLine(myFloatString);
+            Console.Read();
         }
     }
 }
--- 2. DataTypes and variables/strings/Program.cs
+++ 2. DataTypes and variables/strings/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace strings
 {
+    // class names like ClientActivity
     class Program
     {
+        // Method Name like CalculateValue
+        // Method arguments like firstNumber
         static void Main(string[] args)
         {
+            // local variables like itemCount
+            // use userControl instead of usrCtrl 
+
             string myName = "Mantas";
             string message = "My name is " + myName;
             string capsMessage = message.ToUpper();
