2a6e8412669ef61489599c7538272a317ca495f2
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-11-28 19:27:41 +0200

    start

---
+++ 1. First program/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        // starting point of our program
+        static void Main(string[] args)
+        {
+            // Console.ForegroundColor = ConsoleColor.Blue;
+            // Console.BackgroundColor = ConsoleColor.DarkGray;
+            // Console.Clear(); 
+            Console.WriteLine("Hello World");
+            Console.WriteLine("c# is cool");
+            Console.Read();
+        }
+
+        /* This is program
+         * 
+         * 
+         */
+    }
+}
