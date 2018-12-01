3f6d9dccae88b2826b841107a0fc3bbb2c198132
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-01 15:56:34 +0200

    introduction to if

---
+++ 4. Logic and if statements/1. introduction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the temperature");
+            string temp = Console.ReadLine(); 
+            int temperature = int.Parse(temp);
+
+            if (temperature < 10)
+            {
+                Console.WriteLine("Take the coat");
+            }
+
+            if (temperature == 10)
+            {
+                Console.WriteLine("It's 10 degree C");
+            }
+
+            if (temperature > 10)
+            {
+                Console.WriteLine("Cozy warm!");
+            }
+
+            Console.Read();
+        }
+    }
+}
