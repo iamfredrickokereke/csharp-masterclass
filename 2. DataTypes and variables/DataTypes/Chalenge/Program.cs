aa947cd6dc8b23509e6de2040969e43cfb6cd36b
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-01 11:08:13 +0200

    datatypes

---
+++ 2. DataTypes and variables/DataTypes/Chalenge/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chalenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myByte = 25;
+            Console.WriteLine(myByte);
+            sbyte mySbyte = -15;
+            Console.WriteLine(mySbyte);
+            int myInt = 12318925;
+            Console.WriteLine(myInt);
+            uint myUint = 123091;
+            Console.WriteLine(myUint);
+            short myShort = -32768;
+            Console.WriteLine(myShort);
+            ushort myUshort = 65535;
+            Console.WriteLine(myUshort);
+            float myFloat = -31.1289f;
+            Console.WriteLine(myFloat);
+            double myDouble = -12.1231250;
+            Console.WriteLine(myDouble);
+            char myChar = 'A';
+            Console.WriteLine(myChar);
+            bool myBool = true;
+            Console.WriteLine(myBool);
+            string myText = "I control text";
+            Console.WriteLine(myText);
+            string myNum = "15";
+
+            int myNumFromText = int.Parse(myNum);
+            Console.WriteLine(myNumFromText);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2. DataTypes and variables/DataTypes/Constants/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    // Constants are imutable values which are known
+    // At copile time and do not change for the life of the program
+
+    class Program
+    {
+        // constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+        const string birthday = "1990.02.10";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My birthday is always going to be: {0}",  birthday);
+            Console.ReadKey();
+        }
+    }
+}
