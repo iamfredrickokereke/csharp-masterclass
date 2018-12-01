195844df0aa880b81cc475a56bc417b512a7e5cb
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-01 11:46:33 +0200

    void methods

---
+++ 3. Functions and methods/ConsoleApp1/MethodsWithReturnValuesAndParameters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithReturnValuesAndParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- 3. Functions and methods/Methods/Program.cs
+++ 3. Functions and methods/Methods/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Methods
+namespace VoidMethods
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WriteSomething();
+            WriteSomethingSPecific("I am an argument and am called from the method");
+            
+        }
+
+        // access modifier (static) return type method name (parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+   
+        }
+
+        public static void WriteSomethingSPecific(string myText)
+        {
+            Console.WriteLine(myText);
         }
     }
 }
