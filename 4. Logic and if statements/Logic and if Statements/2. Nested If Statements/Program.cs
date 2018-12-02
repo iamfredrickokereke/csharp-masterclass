8e71bf7db6b3e867352a987b9dae0b8ea5134655
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-02 17:37:23 +0200

    login chalenge

--- 4. Logic and if statements/Logic and if Statements/2. Nested If Statements/Program.cs
+++ 4. Logic and if statements/Logic and if Statements/2. Nested If Statements/Program.cs
@@ -11,6 +11,27 @@ namespace _2.Nested_If_Statements
         static void Main(string[] args)
         {
 
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("Please enter your username");
+            userName = Console.ReadLine();
+
+            if (isRegistered && userName != "" && userName.Equals("admin"))
+            {
+                Console.WriteLine("Hi there registered user");
+
+                Console.WriteLine("Hi there, " + userName);
+
+                Console.WriteLine("Hi there, Admin");
+            }
+
+            if(isAdmin || isRegistered)
+            {
+                Console.WriteLine("You are logged in");
+            }
+
+            Console.Read();
         }
     }
 }
