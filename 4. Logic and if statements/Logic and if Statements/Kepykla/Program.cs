4b23206dc9ba899b2350a70289c031aa58b74fdc
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-06 19:59:19 +0200

    kepykla

---
+++ 4. Logic and if statements/Logic and if Statements/Kepykla/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kepykla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valandos = 8;
+            Console.WriteLine("Kiek darbuotojas iskepa kepalu per valanda?");
+            int kepaluPerValanda = int.Parse(Console.ReadLine());
+            Console.WriteLine("Kiek darbuotoju dirba kepykloje");
+            int darbuotojai = int.Parse(Console.ReadLine());
+            Console.WriteLine("Kokia yra vieno kepalo savikaina?");
+            double savikaina = double.Parse(Console.ReadLine());
+            Console.WriteLine("Kokia yra vieno kepalo pardavimo kaina?");
+            double pardavimoKaina = double.Parse(Console.ReadLine());
+            Console.WriteLine("Kiek kepykla turi uzsakymu per diena?");
+            int uzsakymai = int.Parse(Console.ReadLine());
+
+            int visoKepalu = valandos * darbuotojai * kepaluPerValanda;
+            double visuSavikaina = visoKepalu * savikaina;
+            double pajamos = visoKepalu * pardavimoKaina;
+            double pelnas = pajamos - visuSavikaina;
+            int arSpesIskepti = visoKepalu - uzsakymai;
+
+            Console.WriteLine("Kepykla per viena darbo diena iskepa " + visoKepalu + " kepalu");
+            Console.WriteLine("Visu kepalu savikaina yra {0}", visuSavikaina);
+            Console.WriteLine("Visu kepalu pajamos yra {0}", pajamos);
+            Console.WriteLine("Visu kepalu pelnas yra {0}", pelnas);
+
+            if (arSpesIskepti >= 0)
+            {
+                Console.WriteLine("Kepykla spes atlikti uzsakyma");
+            }
+            else
+            {
+                Console.WriteLine("Kepykla nespes atlikti uzsakymo. Jiems pritruks {0} kepalu", arSpesIskepti * -1);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ 4. Loops/Loops/2. Do while/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
