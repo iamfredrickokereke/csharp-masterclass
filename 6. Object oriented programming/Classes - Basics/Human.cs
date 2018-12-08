2bc716f616b99e294639c28f7069c8ec32f33b16
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-08 16:41:38 +0200

    oop bsics

---
+++ 6. Object oriented programming/Classes - Basics/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Basics
+{
+    // this class is a blueprint for a datatype
+    class Human
+    {
+        // member variable
+        public string firstName;
+        public string lastName;
+
+        // member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I am {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ 6. Object oriented programming/Classes - Basics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human sissy = new Human();
+            sissy.firstName = "Sissy";
+            sissy.lastName = "Wagner";
+            sissy.IntroduceMyself();
+
+            Human franzl = new Human();
+            franzl.firstName = "Franzl";
+            franzl.lastName = "Muller";
+            franzl.IntroduceMyself();
+
+            Console.Read();
+        }
+    }
+}
