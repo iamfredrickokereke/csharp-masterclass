27d9febd0db40c2b500f6320badcf7d81d18ee43
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-08 13:32:05 +0200

    fibonacius

---
+++ 4. Loops/Loops/Fibonacius/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kiek fibonaciaus skaiciu norite isvesti?");
+            int kiek = int.Parse(Console.ReadLine());
+
+            Int64 a = 0;
+            Int64 b = 1;
+            Int64 c = 0;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 2; i <= kiek; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+            Console.WriteLine("----------------------");
+            Console.Read();
+
+        }
+    }
+}
