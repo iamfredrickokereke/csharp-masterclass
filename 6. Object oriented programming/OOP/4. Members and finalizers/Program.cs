e95f54820dedcee5d1555be468636b2abf09d707
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-13 19:48:13 +0200

    members

---
+++ 6. Object oriented programming/OOP/4. Members and finalizers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Members_and_finalizers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Members member1 = new Members();
+            member1.Introducing(true);
+
+            Console.Read();
+        }
+    }
+}
