4221678a44e0b414e5f4f3ca4d510a769707a84c
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-12-22 15:58:08 +0200

    multidimensional arrays

---
+++ 7. Arrays/ConsoleApp1/3. Multi dimensiona arrays/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Multi_dimensiona_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare 2d array
+            string[,] matrix;
+
+            //3D array
+            int[,,] treeD;
+
+            // Two dimensional array
+            int[,] array2D = new int[,]
+            {
+                {1,2,3 }, //row 0
+                {4,5,6 }, //row 1
+                {7,8,9 }  //row 2
+            };
+
+            Console.WriteLine("Central value is {0}", array2D[1, 1]);
+            Console.WriteLine("Value is {0}", array2D[2, 0]);
+
+
+            string[,,] array3D = new string[,,]
+            {
+                {
+                    {"000", "001" },
+                    {"010", "011" },
+                    {"Hi there", "What's up" }
+                },
+                {
+                    {"100", "101" },
+                    {"110", "111" },
+                    {"Another one", "Last entry" }
+                }
+            };
+
+            string[,] array2DString = new string[3, 2] 
+            { 
+                { "one", "two", }, 
+                { "three", "four" }, 
+                { "five", "six" }
+            };
+
+            array2DString[1, 1] = "Chicken";
+
+            int dimensions = array2DString.Rank;
+
+            Console.WriteLine("The value is {0}", array3D[1, 2, 1]);
+            Console.WriteLine("The value is {0}", array2DString[1, 1]);
+            Console.WriteLine("Dimensions {0}", dimensions);
+
+
+            
+            Console.Read();
+
+
+        }
+    }
+}
