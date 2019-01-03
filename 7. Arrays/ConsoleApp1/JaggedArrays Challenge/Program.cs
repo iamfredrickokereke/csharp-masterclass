b329b1b774332303529114228527ac05a96c2579
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-01-03 17:33:27 +0200

    chalenge

---
+++ 7. Arrays/ConsoleApp1/JaggedArrays Challenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrays_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a Jagged array, which contains 3 "friends arrays", in which two family members should be storred
+            //Introduce family members form different families to each other via console
+
+            string[][] friendsAndFamily = new string[][]
+            {
+                new string[]{"Michael", "Sandy"},
+                new string[]{"Frank", "Claudia"},
+                new string[]{"Andrew", "Michel"}
+            };
+
+            Console.WriteLine("Hi {0}, I would like to introduce {1} to you.", friendsAndFamily[0][0], friendsAndFamily[1][0]);
+            Console.WriteLine("Hi {0}, I would like to introduce {1} to you.", friendsAndFamily[0][1], friendsAndFamily[2][0]);
+            Console.WriteLine("Hi {0}, I would like to introduce {1} to you.", friendsAndFamily[0][1], friendsAndFamily[2][1]);
+            Console.ReadKey();
+        }
+    }
+}
