1ded1e7511f86f15f865a34637c1411bc8751994
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-01-20 15:31:47 +0200

    write text files

--- 9. Polymorphism/ConsoleApp1/2.How To Read From A TextFile/Program.cs
+++ 9. Polymorphism/ConsoleApp1/2.How To Read From A TextFile/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2.How_To_Read_From_A_TextFile
 {
@@ -10,11 +11,13 @@ namespace _2.How_To_Read_From_A_TextFile
     {
         static void Main(string[] args)
         {
+            /*
             // Example 1 - reading text
             string text = System.IO.File.ReadAllText(@"C:\Users\manta\Documents\csharp-masterclass\9. Polymorphism\textFile.txt");
 
             Console.WriteLine("textFile contains following text: {0}", text);
 
+            // Example 2
             string[] lines = System.IO.File.ReadAllLines(@"C:\Users\manta\Documents\csharp-masterclass\9. Polymorphism\textFile.txt");
 
             Console.WriteLine("Contents of textfile.txt = ");
@@ -22,6 +25,39 @@ namespace _2.How_To_Read_From_A_TextFile
             {
                 Console.WriteLine("\t" + line);
             }
+            */
+
+            // Method 1
+            string[] lines = { "First 250", "Second 242", "Third 240" };
+
+            File.WriteAllLines(@"C:\Users\manta\Documents\csharp-masterclass\9. Polymorphism\highscores.txt", lines);
+
+            // Method 2
+            Console.WriteLine("please give the file a name");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Please enter text for the file");
+            string input = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\manta\Documents\csharp-masterclass\9. Polymorphism\" + fileName + ".txt", input);
+
+            // Method 3
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\manta\Documents\csharp-masterclass\9. Polymorphism\mytext.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    if (line.Contains("2"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
+
+            using(StreamWriter file = new StreamWriter(@"C:\Users\manta\Documents\csharp-masterclass\9. Polymorphism\mytext3.txt", true))
+            {
+                file.WriteLine("Additional line");
+            }
+
             Console.ReadKey();
         }
     }
