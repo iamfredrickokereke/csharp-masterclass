7827e8142d74f06a521ea726d31b737b69dfa8ba
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-01-20 15:13:29 +0200

    reading from text file

---
+++ 9. Polymorphism/ConsoleApp1/2.How To Read From A TextFile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.How_To_Read_From_A_TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1 - reading text
+            string text = System.IO.File.ReadAllText(@"C:\Users\manta\Documents\csharp-masterclass\9. Polymorphism\textFile.txt");
+
+            Console.WriteLine("textFile contains following text: {0}", text);
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\manta\Documents\csharp-masterclass\9. Polymorphism\textFile.txt");
+
+            Console.WriteLine("Contents of textfile.txt = ");
+            foreach(string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+            Console.ReadKey();
+        }
+    }
+}
