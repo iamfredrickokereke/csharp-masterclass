5127016176d6d36b8de4fd62f3d6204fab186311
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-01-20 14:35:35 +0200

    polymorphysm

---
+++ 9. Polymorphism/1. PolymorphysmC/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.PolymorphysmC
+{
+    class Car
+    {
+        public int HP { get; set; }
+        public string Color { get; set; }
+
+
+        public Car(int hp, string color)
+        {
+            this.HP = hp;
+            this.Color = color;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("HP: " + HP + " color: " + Color);
+        }
+
+        public virtual void Repair()
+        {
+            Console.WriteLine("Car was repaired");
+        }
+
+    }
+}
---
+++ 9. Polymorphism/1. PolymorphysmC/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.PolymorphysmC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = new List<Car>
+            {
+                new Audi(200, "blue", "A4"),
+                new BMW(259, "black", "M3")
+            };
+
+            foreach(var car in cars)
+            {
+                car.Repair();
+            }
+
+            Car bmwZ3 = new BMW(200, "black", "Z3");
+            Car audiA3 = new Audi(100, "green", "A3");
+
+            bmwZ3.ShowDetails();
+            audiA3.ShowDetails();
+
+            BMW bmwM5 = new BMW(330, "white", "M5");
+            bmwM5.ShowDetails();
+
+            //Casting
+            Car carB = (Car)bmwM5;
+            carB.ShowDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
