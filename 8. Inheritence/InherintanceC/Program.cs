3d223be497e40ede29c8fc68fd904b8de66faf62
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-01-20 13:03:48 +0200

    inheritance challenge

--- 8. Inheritence/InherintanceC/Program.cs
+++ 8. Inheritence/InherintanceC/Program.cs
@@ -16,6 +16,15 @@ namespace InherintanceC
             ImagePost imagepost1 = new ImagePost("Check out my new shoes", "Denis Panjuta", "https://images.com/shoes", true);
             Console.WriteLine(imagepost1.ToString());
 
+            VideoPost videoPost1 = new VideoPost("Fail video", "Denis Panjuta", true, "https://video.com/failvideo", 5);
+            
+            Console.WriteLine(videoPost1.ToString());
+
+            videoPost1.Play();
+            Console.WriteLine("Press any key to stop the video");
+            Console.ReadKey();
+            videoPost1.Stop();
+
             Console.ReadLine();
         }
     }
---
+++ 8. Inheritence/InherintanceC/VideoPost.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InherintanceC
+{
+    class VideoPost : Post
+    {
+        //member fields
+        protected bool isPlaying = false;
+        protected int currDuration = 0;
+        Timer timer;
+
+        //properties
+        protected string VideoURL { get; set; }
+        protected int Length { get; set; }
+
+
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, bool isPublic, string videoURL, int length)
+        {
+            //Inherited from Post
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+
+            //propertiy VideoUrl is a member of VideoPost
+            this.VideoURL = videoURL;
+            this.Length = length;
+        }
+
+        public void Play()
+        {
+            if(!isPlaying)
+            {
+                isPlaying = true;
+                Console.WriteLine("Playing");
+                timer = new Timer(TimerCallBack, null, 0, 1000);
+            }
+
+        }
+
+        private void TimerCallBack(Object o)
+        {
+            if(currDuration < Length)
+            {
+                currDuration++;
+                Console.WriteLine("Video at {0}s", currDuration);
+                GC.Collect();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        public void Stop()
+        {
+            if(isPlaying)
+            {
+                isPlaying = false;
+                Console.WriteLine("Stopped at {0}", currDuration);
+                currDuration = 0;
+                timer.Dispose();
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - {3} - by {4}",
+                this.ID, this.Title, this.VideoURL, this.Length, this.SendByUsername);
+        }
+    }
+}
