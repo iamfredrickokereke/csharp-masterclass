c0213d786eac0c15d35dcb0fc243f21d0546802c
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-01-30 16:20:09 +0200

    little calculator

--- 11. WPF/WPF 01C/MainWindow.xaml.cs
+++ 11. WPF/WPF 01C/MainWindow.xaml.cs
@@ -20,85 +20,17 @@ namespace WPF_01C
     /// </summary>
     public partial class MainWindow : Window
     {
-        /*
-        public int MyProperty
-        {
-            get
-            {
-                return (int)GetValue(myDependencyProperty);
-            }
-            set
-            {
-                SetValue(myDependencyProperty, value);
-            }
-        }
+        public Sum SumObj { get; set; }
 
-        public static readonly DependencyProperty myDependencyProperty = DependencyProperty.Register("My Property", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
-        */
+
         public MainWindow()
         {
             InitializeComponent();
-            MySlider.Value = 30;
-            MyTextBox.Text = MySlider.Value.ToString();
 
-            /*
-            InitializeComponent();
-            Grid grid = new Grid();
-            this.Content = grid;
-
-            Button btn = new Button();
-
-            btn.Width = 150;
-            btn.Height = 100;
-            btn.FontSize = 26;
-
-           
-            WrapPanel wrapPanel = new WrapPanel();
-            TextBlock txt = new TextBlock();
-            txt.Text = "Multi";
-            txt.Foreground = Brushes.Blue;
-            wrapPanel.Children.Add(txt);
-
-            txt = new TextBlock();
-            txt.Text = "Color";
-            txt.Foreground = Brushes.Red;
-            wrapPanel.Children.Add(txt);
-
-            txt = new TextBlock();
-            txt.Text = "Button";
-            txt.Foreground = Brushes.White;
-            wrapPanel.Children.Add(txt);
-
-            btn.Content = wrapPanel;
-            grid.Children.Add(btn);
-            */
+            SumObj = new Sum { Num1 = "1", Num2 = "3" };
+            this.DataContext = SumObj;
         }
 
-        /*
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Button was clicked - Direct Event");
-        }
 
-        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Mouse button went up / was realesed - Bubbling");
-        }
-
-        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Mouse button went up / was realesed - tunneling event");
-        }
-
-        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Left Mouse button went down - tunneling event");
-        }
-
-        private void Button_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("RighrMouse button went up - tunneling event");
-        }
-        */
     }
 }
